d1e1594d148c334f2d52a4e1ffd4b050fe3c6d32
Ivan <ivanremeslennikov@yandex.ru>
2021-01-04 00:53:21 +0400

    Немного улучшил производительность и добавил немного эфектов, но программа по прежнему сырая и ничего толком не делает

--- main_soft.py
+++ main_soft.py
@@ -1,36 +1,53 @@
 import random
 import pygame
 import os
+import numpy as np
+from time import perf_counter
 
 from sprites import *
 
 
 class PlayingBoard:
-    space = pygame.sprite.Sprite()
-    space.image = pygame.Surface((64, 64))
-    pygame.draw.rect(space.image, (0, 0, 0), pygame.Rect(0, 0, 64, 64))
-    space.rect = space.image.get_rect()
-
-    grow = pygame.sprite.Sprite()
-    grow.image = pygame.image.load(os.path.join('sprite_1.png'))
-    grow.rect = grow.image.get_rect()
-    TOC = {
-        '_': space,
-        '1': grow
-    }
-
-    def __init__(self, screen, filename):
+    def __init__(self, screen, map_filename, images):
         self.screen = screen
-        self.map = self.load_map(filename)
-        self.focus = [10, 11]  # y_x
+        self.map = self.load_map(map_filename)
+        self.toc = self.create_toc(images)
+
+        self.nps_group = pygame.sprite.Group()
+        self.mobs_group = pygame.sprite.Group()
+        self.hero = pygame.sprite.Group()
+
+        self.focus = [10, 40]  # y_x
         self.width = 19
         self.height = 15
-        self.player = Player(global_pos=[640 + 19 * 32, 640 + 15 * 32], cell_pos=[0, 32])
+        self.player = Player(self.focus, [self.focus[1] * 64 + 19 * 32, self.focus[0] * 64 + 15 * 32], [10, 32],
+                             self.hero)
 
     def load_map(self, filename):
-        output = []
+        interim = []
         with open(filename, 'r') as file:
-            output = [i.rstrip('\n') for i in file.readlines()]
+            interim = [i.rstrip('\n') for i in file.readlines()]
+
+        output = np.zeros((30, 100), dtype=int)
+        for y, mas in enumerate(interim):
+            for x, mark in enumerate(mas):
+                if mark != '_':
+                    output[y, x] = int(mark)
+
+        return output
+
+    def create_toc(self, images):
+        output = {}
+
+        for name in images:
+            image = pygame.image.load(os.path.join(name))
+            name = name.rstrip('.png')
+            output[int(name.split('_')[-1])] = image
+
+        space = pygame.Surface((64, 64))
+        pygame.draw.rect(space, (0, 0, 255), pygame.Rect(0, 0, 64, 64))
+        output[0] = space
+
         return output
 
     def render(self):
@@ -38,22 +55,38 @@ class PlayingBoard:
         area = pygame.sprite.Group()
         for y in range(-1, self.height + 1):
             for x in range(-1, self.width + 1):
-                if self.map[y + yd][x + xd] == '1':
-                    grow = pygame.sprite.Sprite()
-                    grow.image = pygame.image.load(os.path.join('sprite_1.png'))
-                    grow.rect = grow.image.get_rect()
-                    sprite = grow
-                else:
-                    space = pygame.sprite.Sprite()
-                    space.image = pygame.Surface((64, 64))
-                    pygame.draw.rect(space.image, (0, 0, 0), pygame.Rect(0, 0, 64, 64))
-                    space.rect = space.image.get_rect()
-                    sprite = space
+                sprite = pygame.sprite.Sprite()
+                sprite.image = self.toc[self.map[y + yd, x + xd]]
+                sprite.rect = sprite.image.get_rect()
                 sprite.rect.x = x * 64 + (32 - self.player.cell_pos[0])
                 sprite.rect.y = y * 64 + (32 - self.player.cell_pos[1])
                 area.add(sprite)
 
         area.draw(self.screen)
+        self.mobs_group.draw(self.screen)
+        self.nps_group.draw(self.screen)
+        self.hero.draw(self.screen)
+
+
+class Camera:
+    def __init__(self, screen):
+        self.group = pygame.sprite.Group()
+        self.screen = screen
+        self.mouse_pos = (0, 0)  # x_y
+        self.cursor = pygame.sprite.Sprite(self.group)
+        self.cursor.image = pygame.image.load(os.path.join('cursor.png'))
+        self.cursor.rect = self.cursor.image.get_rect()
+        self.cursor.rect.x = 0
+        self.cursor.rect.y = 0
+
+    def update_mouse(self, pos):
+        self.mouse_pos = pos
+        self.cursor.rect.x = pos[0]
+        self.cursor.rect.y = pos[1]
+
+    def draw(self):
+        if pygame.mouse.get_focused():
+            self.group.draw(self.screen)
 
 
 def main():
@@ -61,9 +94,11 @@ def main():
     size = width, height = 1216, 960
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('protoDiablo')
+    pygame.mouse.set_visible(False)
 
     FPS = 30
-    board = PlayingBoard(screen, 'map.txt')
+    board = PlayingBoard(screen, 'map.txt', ('sprite_1.png',))
+    camera = Camera(screen)
     clock = pygame.time.Clock()
 
     running = True
@@ -71,9 +106,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEMOTION:
+                mouse_pos = event.pos
+                camera.update_mouse(mouse_pos)
 
         screen.fill((0, 255, 0))
         board.render()
+        camera.draw()
         pygame.display.flip()
         clock.tick(FPS)
 
--- sprites.py
+++ sprites.py
@@ -1,13 +1,19 @@
 from pygame.sprite import Sprite, Group
+import pygame
+import os
 
 
 class Object(Sprite):
-    def __init__(self):
-        super().__init__()
-        pass
+    def __init__(self, global_pos, cell_pos, *args):
+        super().__init__(*args)
+        self.global_pos = global_pos
+        self.cell_pos = cell_pos
 
 
 class Player(Object):
-    def __init__(self, global_pos, cell_pos):
-        super().__init__()
-        self.cell_pos = cell_pos
+    def __init__(self, focus, *args):
+        super().__init__(*args)
+        self.image = pygame.image.load(os.path.join('player.png'))
+        self.rect = self.image.get_rect()
+        self.rect.x = self.global_pos[0] - focus[1] * 64
+        self.rect.y = self.global_pos[1] - focus[0] * 64
